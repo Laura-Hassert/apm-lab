ded1dd313da60a89e91e8e0c33a23b3b0ef034ce
Laura-Hassert <laura.hassert@gmail.com>
2021-10-12 15:56:20 -0400

    adding rollbar tests

--- server.js
+++ server.js
@@ -54,54 +54,55 @@ let characters = [
     }
 ]
 
-// app.get('/characters', (req, res) => {
-//     rollbar.error
-//     res.status(200).send(characters)
-// })
 
 app.get('/characters', (req, res) => {
     console.log('hit chars')
     res.status(200).send(characters)
 })
 
-app.get('/character/:name', (req, res) => {
-    const { name } = req.params
-    const index = characters.findIndex(char => char.firstName.toLowerCase() === name)
-    res.status(200).send(characters[index])
-})
+// app.get('/character/:name', (req, res) => {
+//     const { name } = req.params
+//     const index = characters.findIndex(char => char.firstName.toLowerCase() === name)
+//     res.status(200).send(characters[index])
+// })
 
 app.get('/character', (req, res) => {
     const { age } = req.query
-    
-    let filtered = characters.filter(char => {
+
+    if(age != ''){
+        let filtered = characters.filter(char => {
         return char.age > age
-    })
-
-    res.status(200).send(filtered)
-})
-
-let id = 4
-
-app.post('/character', (req, res) => {
-    let newChar = {...req.body, id}
-    newChar.likes = newChar.likes.slice(0, 3)
-    
-    const index = characters.findIndex(characterName => characterName = firstName)
-    
-    if(index === -1 && firstName != ''){
-        characters.unshift(newChar)
-        rollbar.log('New character added!', {author:'Laura', type:'manual entry'})
-        res.status(200).send(characters)
-        id++
-    } else if (req.body === ''){
-        rollbar.error('Not enoug character information')
-        res.status(400).send('Please complete all required fields')
+        })
+        rollbar.log('age limit valid')
+        res.status(200).send(filtered)
     } else {
-        rollbar.error('Character already exists')
-        res.status(400).send('This character already exists')
+        rollbar.error('no age entered')
+        res.status(400).send('please enter an age to search')
     }
-
 })
+
+// let id = 4
+
+// app.post('/character', (req, res) => {
+//     let newChar = {...req.body, id}
+//     newChar.likes = newChar.likes.slice(0, 3)
+    
+//     const index = characters.findIndex(characterName => characterName = firstName)
+    
+//     if(index === -1 && firstName != ''){
+//         characters.unshift(newChar)
+//         rollbar.log('New character added!', {author:'Laura', type:'manual entry'})
+//         res.status(200).send(characters)
+//         id++
+//     } else if (req.body === ''){
+//         rollbar.error('Not enoug character information')
+//         res.status(400).send('Please complete all required fields')
+//     } else {
+//         rollbar.error('Character already exists')
+//         res.status(400).send('This character already exists')
+//     }
+
+// })
 
 app.use(rollbar.errorHandler())
 
